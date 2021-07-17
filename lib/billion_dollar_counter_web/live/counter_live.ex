6ba6227847cb8f8a1270c573530f8f89ceb530fa
Burak <burak@llinformatics.com>
2021-07-17 23:44:46 +0200

    Add pub/sub to broadcast counter value

--- lib/billion_dollar_counter_web/live/counter_live.ex
+++ lib/billion_dollar_counter_web/live/counter_live.ex
@@ -3,16 +3,26 @@ defmodule BillionDollarCounterWeb.CounterLive do
 
   alias BillionDollarCounter.Counter
 
+  @topic "counter"
+
   @impl true
   def mount(_params, _session, socket) do
+    BillionDollarCounterWeb.Endpoint.subscribe(@topic)
     counter_value = Counter.value()
     {:ok, assign(socket, counter_value: counter_value)}
   end
 
   @impl true
-  def handle_event("inc", _, socket) do
+  def handle_event("inc", _session, socket) do
     Counter.increment()
     counter_value = Counter.value()
-    {:noreply, assign(socket, counter_value: counter_value)}
+    socket = assign(socket, counter_value: counter_value)
+    BillionDollarCounterWeb.Endpoint.broadcast(@topic, "inc", %{counter_value: counter_value})
+
+    {:noreply, socket}
+  end
+
+  def handle_info(%{event: "inc", payload: payload} = msg, socket) do
+    {:noreply, assign(socket, payload)}
   end
 end
