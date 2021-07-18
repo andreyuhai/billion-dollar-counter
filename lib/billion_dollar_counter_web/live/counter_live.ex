396ec76874270a5affde8123eea64af0141c385e
Burak <burak@llinformatics.com>
2021-07-18 13:26:08 +0200

    Start tracking Presence in mount

--- lib/billion_dollar_counter_web/live/counter_live.ex
+++ lib/billion_dollar_counter_web/live/counter_live.ex
@@ -2,14 +2,32 @@ defmodule BillionDollarCounterWeb.CounterLive do
   use BillionDollarCounterWeb, :live_view
 
   alias BillionDollarCounter.Counter
+  alias BillionDollarCounterWeb.Presence
 
   @topic "counter"
 
   @impl true
   def mount(_params, _session, socket) do
     BillionDollarCounterWeb.Endpoint.subscribe(@topic)
+    case get_connect_info(socket) do
+      %{} = info ->
+        Presence.track(
+          self(),
+          @topic,
+          info.peer_data.address |> :inet.ntoa(),
+          %{
+            online_at: inspect(System.system_time(:second))
+          }
+        )
+
+      _nil -> true
+    end
     counter_value = Counter.value()
-    {:ok, assign(socket, counter_value: counter_value)}
+    {:ok,
+      socket
+      |> assign(:counter_value, counter_value)
+      |> assign(:presence_list, Presence.list(@topic))
+    }
   end
 
   @impl true
