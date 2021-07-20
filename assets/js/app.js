a008441fab3f0287519b8e17ce9bbe6160d7c826
Burak <burak@llinformatics.com>
2021-07-20 15:12:04 +0200

    Move let Hooks = {} to app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -18,6 +18,9 @@ import topbar from "topbar"
 import {LiveSocket} from "phoenix_live_view"
 import {Hooks} from  "./chart"
 
+let Hooks = {};
+Hooks.Chart = Chart;
+
 let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content")
 let liveSocket = new LiveSocket("/live", Socket, {params: {_csrf_token: csrfToken}, hooks: Hooks})
 
