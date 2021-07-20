a9f897e8b23ddb8e279a394d0b81e0915ed31152
Burak <burak@llinformatics.com>
2021-07-20 15:20:21 +0200

    Rename the hook for chart update

--- assets/js/app.js
+++ assets/js/app.js
@@ -16,10 +16,10 @@ import "phoenix_html"
 import {Socket} from "phoenix"
 import topbar from "topbar"
 import {LiveSocket} from "phoenix_live_view"
-import {Hooks} from  "./chart"
+import ChartUpdateHook from "./chart"
 
 let Hooks = {};
-Hooks.Chart = Chart;
+Hooks.ChartUpdate = ChartUpdateHook;
 
 let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content")
 let liveSocket = new LiveSocket("/live", Socket, {params: {_csrf_token: csrfToken}, hooks: Hooks})
