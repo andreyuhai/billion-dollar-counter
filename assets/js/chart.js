619d7817bb7d3f2a44701ee2e5d449e628968e71
Burak <burak@llinformatics.com>
2021-07-20 09:50:45 +0200

    Add amcharts map

---
+++ assets/js/chart.js
@@ -0,0 +1,57 @@
+import * as am4core from "@amcharts/amcharts4/core";
+import * as am4maps from "@amcharts/amcharts4/maps";
+import am4geodata_worldLow from "@amcharts/amcharts4-geodata/worldLow";
+
+am4core.options.autoDispose = true;
+var chart = am4core.create("chartdiv", am4maps.MapChart);
+chart.maxZoomLevel = 64;
+
+// Set map definition
+chart.geodata = am4geodata_worldLow;
+
+// Set projection
+chart.projection = new am4maps.projections.Miller();
+
+// Create map polygon series
+var polygonSeries = chart.series.push(new am4maps.MapPolygonSeries());
+polygonSeries.useGeodata = true;
+polygonSeries.calculateVisualCenter = true;
+
+// Configure series
+var polygonTemplate = polygonSeries.mapPolygons.template;
+polygonTemplate.tooltipText = "{name}";
+polygonTemplate.fill = chart.colors.getIndex(0);
+
+polygonSeries.exclude = ["AQ"];
+
+// Create image series
+var imageSeries = chart.series.push(new am4maps.MapImageSeries());
+
+// Create a circle image in image series template so it gets replicated to all new images
+var imageSeriesTemplate = imageSeries.mapImages.template;
+var circle = imageSeriesTemplate.createChild(am4core.Circle);
+circle.radius = 4;
+circle.fill = am4core.color("#B27799");
+circle.stroke = am4core.color("#FFFFFF");
+circle.strokeWidth = 2;
+circle.nonScaling = true;
+circle.tooltipText = "{title}";
+
+// Set property fields
+imageSeriesTemplate.propertyFields.latitude = "latitude";
+imageSeriesTemplate.propertyFields.longitude = "longitude";
+
+// Add data for the three cities
+imageSeries.data = [{
+  "latitude": 48.856614,
+  "longitude": 2.352222,
+  "title": "Paris"
+}, {
+  "latitude": 40.712775,
+  "longitude": -74.005973,
+  "title": "New York"
+}, {
+  "latitude": 49.282729,
+  "longitude": -123.120738,
+  "title": "Vancouver"
+}];
