e115aa40ccb973f709eec6340831526246f5fab5
Burak <burak@llinformatics.com>
2021-07-23 00:03:37 +0200

    Overwrite all the JS file to be able to create a better map

--- assets/js/chart/chart.js
+++ assets/js/chart/chart.js
@@ -1,88 +1,136 @@
 import * as am4core from "@amcharts/amcharts4/core";
 import * as am4maps from "@amcharts/amcharts4/maps";
 import am4geodata_worldLow from "@amcharts/amcharts4-geodata/worldLow";
+import am4geodata_data_countries2 from "@amcharts/amcharts4-geodata/data/countries2";
+import am4themes_animated from "@amcharts/amcharts4/themes/animated";
 
-am4core.options.autoDispose = true;
+// Use animation, when loading map etc.
+am4core.useTheme(am4themes_animated);
+
+var continents = {
+  "AF": 0,
+  "AN": 1,
+  "AS": 2,
+  "EU": 3,
+  "NA": 4,
+  "OC": 5,
+  "SA": 6
+}
+
+// Create map instance
 var chart = am4core.create("chartdiv", am4maps.MapChart);
-chart.maxZoomLevel = 64;
 
-// Set map definition
-chart.geodata = am4geodata_worldLow;
-
-// Set projection
-chart.projection = new am4maps.projections.Miller();
-
-// Add button
-var zoomOut = chart.tooltipContainer.createChild(am4core.ZoomOutButton);
-zoomOut.align = "right";
-zoomOut.valign = "top";
-zoomOut.margin(20, 20, 20, 20);
-zoomOut.events.on("hit", function() {
-  if (currentSeries) {
-    currentSeries.hide();
-  }
-  chart.goHome();
-  zoomOut.hide();
-  currentSeries = worldSeries.series;
-  currentSeries.show();
-});
-zoomOut.hide();
-
-// Create map polygon series
-var polygonSeries = chart.series.push(new am4maps.MapPolygonSeries());
-polygonSeries.useGeodata = true;
-polygonSeries.calculateVisualCenter = true;
-
-// Configure series
-var polygonTemplate = polygonSeries.mapPolygons.template;
-polygonTemplate.tooltipText = "{name}";
-polygonTemplate.fill = chart.colors.getIndex(0);
-
-polygonSeries.exclude = ["AQ"];
-
+// Set map title
 var title = chart.titles.create();
 title.text = "Live User Map";
 title.fontSize = 25;
 title.marginBottom = 30;
 
-// Create image series
-var imageSeries = chart.series.push(new am4maps.MapImageSeries());
+// Set map definition
+chart.geodata = am4geodata_worldLow;
 
-// Create a circle image in image series template so it gets replicated to all new images
-var imageSeriesTemplate = imageSeries.mapImages.template;
-var circle = imageSeriesTemplate.createChild(am4core.Circle);
-circle.radius = 4;
-circle.fill = am4core.color("#34B211");
-circle.stroke = am4core.color("#FFFFFF");
-circle.strokeWidth = 2;
-circle.nonScaling = true;
-circle.tooltipText = "{title}";
+// Set map projection
+chart.projection = new am4maps.projections.Miller();
 
-// Set property fields
-imageSeriesTemplate.propertyFields.latitude = "latitude";
-imageSeriesTemplate.propertyFields.longitude = "longitude";
+// Create map polygon series for world map
+var worldSeries = chart.series.push(new am4maps.MapPolygonSeries());
+worldSeries.useGeodata = true;
+worldSeries.calculateVisualCenter = true;
+worldSeries.exclude = ["AQ"];
 
-// Add data for the three cities
-imageSeries.data = [];
+// Configure worldSeries polygons
+var worldPolygon = worldSeries.mapPolygons.template;
+worldPolygon.tooltipText = "{name}";
+worldPolygon.fill = am4core.color("#eee");
+
+// Fill worldPolygon on hover
+var hs = worldPolygon.states.create("hover");
+hs.properties.fill = chart.colors.getIndex(9);
+
+// Create country specific series (but hide it for now)
+var countrySeries = chart.series.push(new am4maps.MapPolygonSeries());
+countrySeries.useGeodata = true;
+countrySeries.hide();
+countrySeries.geodataSource.events.on("done", function(ev) {
+  worldSeries.hide();
+  countrySeries.show();
+});
+
+// Configure countrySeries polygons
+var countryPolygon = countrySeries.mapPolygons.template;
+countryPolygon.tooltipText = "{name}";
+countryPolygon.nonScalingStroke = true;
+countryPolygon.strokeOpacity = 0.5;
+countryPolygon.fill = am4core.color("#eee");
+
+// Fill countryPolygon on hover
+var hs = countryPolygon.states.create("hover");
+hs.properties.fill = chart.colors.getIndex(9);
+
+// Set up data for countries
+var data = [];
+for (var id in am4geodata_data_countries2) {
+  if (am4geodata_data_countries2.hasOwnProperty(id)) {
+    var country = am4geodata_data_countries2[id];
+    if (country.maps.length) {
+      data.push({
+        id: id,
+        color: chart.colors.getIndex(continents[country.continent_code]),
+        map: country.maps[0]
+      });
+    }
+  }
+}
+
+worldSeries.data = data;
+
+// Zoom control
+chart.zoomControl = new am4maps.ZoomControl();
+
+var homeButton = new am4core.Button();
+homeButton.events.on("hit", function() {
+  worldSeries.show();
+  countrySeries.hide();
+
+  if (currentSeries) {
+    currentSeries.hide();
+  }
+
+  if (onlineUserSeries.series) {
+    currentSeries = onlineUserSeries.series;
+    currentSeries.show();
+  }
+
+  chart.goHome();
+});
+
+homeButton.icon = new am4core.Sprite();
+homeButton.padding(7, 5, 7, 5);
+homeButton.width = 30;
+homeButton.icon.path = "M16,8 L14,8 L14,16 L10,16 L10,10 L6,10 L6,16 L2,16 L2,8 L0,8 L8,0 L16,8 Z M16,8";
+homeButton.marginBottom = 10;
+homeButton.parent = chart.zoomControl;
+homeButton.insertBefore(chart.zoomControl.plusButton);
 
 // Drill-down map
-
-
 // Creates a series
-function createSeries(heatfield) {
+function createSeries(name) {
+  removeSeriesByName(name);
   var series = chart.series.push(new am4maps.MapImageSeries());
-  series.dataFields.value = heatfield;
+  series.name = name;
 
   var template = series.mapImages.template;
   template.verticalCenter = "middle";
   template.horizontalCenter = "middle";
   template.propertyFields.latitude = "lat";
   template.propertyFields.longitude = "long";
-  template.tooltipText = "{name}:\n[bold]{onlineUsers} online user[/]";
+  template.fill = am4core.color("#eee");
+  template.tooltipText = "{name}: {onlineUsers} online user";
 
   var circle = template.createChild(am4core.Circle);
   circle.radius = 10;
-  circle.fillOpacity = 0.7;
+  // circle.fillOpacity = 0.7;
+  circle.fill = am4core.color("#3EC186");
   circle.verticalCenter = "middle";
   circle.horizontalCenter = "middle";
   circle.nonScaling = true;
@@ -112,64 +160,62 @@ function createSeries(heatfield) {
       return;
     }
 
-    // Create actual series if it hasn't been yet created
-    if (!worldSeries[data.target].series) {
-      worldSeries[data.target].series = createSeries("count");
-      worldSeries[data.target].series.data = data.markerData;
-    }
-
-    // Hide current series
-    if (currentSeries) {
-      currentSeries.hide();
-    }
-
-    // Control zoom
     if (data.type == "country") {
-      var statePolygon = polygonSeries.getPolygonById(data.country_code);
-      chart.zoomToMapObject(statePolygon);
-    }
-    else if (data.type == "city") {
-      chart.zoomToGeoPoint({
-        latitude: data.lat,
-        longitude: data.long
-      }, 64, true);
-    }
-    zoomOut.show();
+      // Create actual series if it hasn't been yet created
+      if (!onlineUserSeries[data.target].series) {
+        onlineUserSeries[data.target].series = createSeries(`${data.target}`);
+        onlineUserSeries[data.target].series.data = data.markerData;
+      }
 
-    // Show new targert series
-    currentSeries = worldSeries[data.target].series;
-    console.log("Inside createSeries");
-    console.log(currentSeries);
-    window.currentSeries = currentSeries;
-    window.chart = chart;
-    currentSeries.show();
+      // Hide current series
+      if (currentSeries) {
+        currentSeries.hide();
+      }
+
+      // Control zoom
+      var countryPolygon = worldSeries.getPolygonById(data.country_code);
+      chart.zoomToMapObject(countryPolygon);
+      var map = countryPolygon.dataItem.dataContext.map;
+
+      if (map) {
+        countryPolygon.isHover = false;
+        countrySeries.geodataSource.url = "https://www.amcharts.com/lib/4/geodata/json/" + map + ".json";
+        countrySeries.geodataSource.load();
+      }
+
+      // Show new targert series
+      currentSeries = onlineUserSeries[data.target].series;
+      window.currentSeries = currentSeries;
+      window.chart = chart;
+      currentSeries.show();
+    }
   });
-
   return series;
 }
 
-var worldSeries = {};
+var onlineUserSeries = {};
 var currentSeries;
 
-function setupOnlineUsers(data) {
+function removeSeriesByName(name) {
+  if (chart.series.values.find(elem => elem.name == name)) {
+    chart.series.removeIndex(
+      chart.series.indexOf(chart.series.values.find(elem => elem.name == name))
+    ).dispose();
+  };
+}
 
+function setupOnlineUsers(data) {
   // Init country-level series
-  worldSeries = {
+  onlineUserSeries = {
     markerData: [],
-    series: createSeries("locations")
+    series: createSeries("world-level")
   };
 
   // Set current series
-  currentSeries = worldSeries.series;
-
-  console.log("SETUP START")
-  window.currentSeries = currentSeries;
-  console.log(chart)
-  console.log(currentSeries)
+  currentSeries = onlineUserSeries.series;
 
   // Process data
   am4core.array.each(data, function(onlineUser) {
-
     // Get user data
     var onlineUser = {
       long: onlineUser.longitude,
@@ -181,13 +227,13 @@ function setupOnlineUsers(data) {
     };
 
     // Process world-level data
-    if (worldSeries[onlineUser.country_code] == undefined) {
-      var countryPolygon = polygonSeries.getPolygonById(onlineUser.country_code);
+    if (onlineUserSeries[onlineUser.country_code] == undefined) {
+      var countryPolygon = worldSeries.getPolygonById(onlineUser.country_code);
       if (countryPolygon) {
 
         // Add world data
-        worldSeries[onlineUser.country_code] = {
-	  country_code: onlineUser.country_code,
+        onlineUserSeries[onlineUser.country_code] = {
+          country_code: onlineUser.country_code,
           target: onlineUser.country_code,
           type: "country",
           name: countryPolygon.dataItem.dataContext.name,
@@ -197,22 +243,18 @@ function setupOnlineUsers(data) {
           country: onlineUser.country_code,
           markerData: []
         };
-	console.log("Here")
-	console.log(worldSeries)
-        worldSeries.markerData.push(worldSeries[onlineUser.country_code]);
-      }
-      else {
+        onlineUserSeries.markerData.push(onlineUserSeries[onlineUser.country_code]);
+      } else {
         // Country not found
         return;
       }
-    }
-    else {
-      worldSeries[onlineUser.country_code].onlineUsers++;
+    } else {
+      onlineUserSeries[onlineUser.country_code].onlineUsers++;
     }
 
     // Process country-level data
-    if (worldSeries[onlineUser.city] == undefined) {
-      worldSeries[onlineUser.city] = {
+    if (onlineUserSeries[onlineUser.city] == undefined) {
+      onlineUserSeries[onlineUser.city] = {
         target: onlineUser.city,
         type: "city",
         name: onlineUser.city,
@@ -222,37 +264,24 @@ function setupOnlineUsers(data) {
         state: onlineUser.state,
         markerData: []
       };
-      worldSeries[onlineUser.country_code].markerData.push(worldSeries[onlineUser.city]);
+      onlineUserSeries[onlineUser.country_code].markerData.push(onlineUserSeries[onlineUser.city]);
+    } else {
+      onlineUserSeries[onlineUser.city].onlineUsers++;
     }
-    else {
-      worldSeries[onlineUser.city].onlineUsers++;
-    }
-
-    // Process individual onlineUser
-    worldSeries[onlineUser.city].markerData.push({
-      city: onlineUser.city,
-      onlineUsers: 1,
-      lat: onlineUser.lat,
-      long: onlineUser.long,
-      country_code: onlineUser.country_code
-    });
-
   });
 
-  console.log("SETUP END")
+  console.log("worldSeries after")
+  console.log(onlineUserSeries)
   window.currentSeries = currentSeries;
-  console.log(chart)
-  console.log(currentSeries)
-  console.log(worldSeries)
-  worldSeries.series.data = worldSeries.markerData;
+  window.chart = chart;
+  window.onlineUserSeries = onlineUserSeries;
+  onlineUserSeries.series.data = onlineUserSeries.markerData;
 }
-
-// Drill-down map end
 
 // Hooks
 const chartUpdatedHook = {
   mounted() {
-    this.handleEvent("update_presence_list", ({presence_list}) => { console.log(presence_list); setupOnlineUsers(presence_list) })
+    this.handleEvent("update_presence_list", ({presence_list}) => { setupOnlineUsers(presence_list) })
   }
 }
 
